71313e9d76aadc9d0524c38b922209be97d2cf40
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-29 21:19:37 +0530

    Add project files.

---
+++ TestingRegex/Pattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegestrationRegex
+{
+    public class Pattern
+    {
+
+        private string firstNameRegex = "^[A-Z][a-zA-Z]{2,}$";
+        private string lastNameRegex = "^[A-Z][a-zA-Z]{2,}$";
+        private string emailRegex = "^[a-zA-Z]{3}[.]([a-zA-z]{3})[@][a-zA-Z]{2}[.][a-zA-Z]{2}[.]([a-zA-Z]{2})";
+        private string mobileRegex = "^[1-9]{2,2}[ ][0-9]{10}$";
+        private string passwordEmailRegex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
+        public bool firstNameCheck(string input)
+        {
+            return Regex.IsMatch(input, firstNameRegex);
+        }
+        public bool lastNameCheck(string input)
+        {
+            return Regex.IsMatch(input, lastNameRegex);
+        }
+        public bool EmailCheck(string input)
+        {
+            return Regex.IsMatch(input, emailRegex);
+        }
+        public bool MobileCheck(string input)
+        {
+            return Regex.IsMatch(input, mobileRegex);
+        }
+        public bool PasswordEmailCheck(string input)
+        {
+            return Regex.IsMatch(input, passwordEmailRegex);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegestrationRegex;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Pattern pattern = new Pattern();
+        [TestMethod]
+        public void TestMethod1()
+        {
+           bool res= pattern.firstNameCheck("Prashant");
+            Assert.IsTrue(res);
+           
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool res=pattern.lastNameCheck("Bhure");
+            Assert.IsTrue(res);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+          bool res =   pattern.EmailCheck("abc.xyz@bl.co.in");
+            Assert.IsTrue(res);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+           bool res =  pattern.MobileCheck("91 7000593588");
+            Assert.IsTrue(res);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            bool res = pattern.PasswordEmailCheck("@bcdEfghi1");
+            Assert.IsTrue(res);
+        }
+        
+    }
+}
